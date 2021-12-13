0443f33dda379d98610428a713ce06129491daa8
vkovaleva <vkovaleva@voxelgroup.net>
2021-12-13 17:55:38 +0100

    refactor: moved Token and Coordinates to a separate classes

--- TicTacToeKata/Board.cs
+++ TicTacToeKata/Board.cs
@@ -2,33 +2,6 @@
 
 namespace TicTacToeKata
 {
-    public class Token
-    {
-        public Token(char value)
-        {
-            Value = value;
-        }
-
-        public char Value { get; private set; }
-
-        public bool IsInvalidToken()
-        {
-            return Value != 'x' && Value != 'o';
-        }
-    }
-
-    public class Coordinates
-    {
-        public Coordinates(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public int X { get; private set; }
-        public int Y { get; private set; }
-    }
-
     public class Board
     {
         private char _playingToken;
---
+++ TicTacToeKata/Token.cs
@@ -0,0 +1,17 @@
+﻿namespace TicTacToeKata
+{
+    public class Token
+    {
+        public Token(char value)
+        {
+            Value = value;
+        }
+
+        public char Value { get; private set; }
+
+        public bool IsInvalidToken()
+        {
+            return Value != 'x' && Value != 'o';
+        }
+    }
+}
