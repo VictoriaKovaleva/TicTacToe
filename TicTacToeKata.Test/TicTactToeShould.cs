062e6ea796ded5d17cd0b45cb180c9d11be7e6b0
vkovaleva <vkovaleva@voxelgroup.net>
2021-12-13 17:25:34 +0100

    refactor: generalise tests for tokens not x or o into theory

--- TicTacToeKata.Test/TicTactToeShould.cs
+++ TicTacToeKata.Test/TicTactToeShould.cs
@@ -13,29 +13,20 @@ namespace TicTacToeKata.Test
             Board board = new Board();
 
             board.PlaceToken(token, 0, 0);
-                
-            Assert.Equal(token, board.GetTokenAtPosition(0,0));
+
+            Assert.Equal(token, board.GetTokenAtPosition(0, 0));
         }
 
-        [Fact]
-        public void NotAllowPlayerToPlaceTokenD()
+        [Theory]
+        [InlineData('d')]
+        [InlineData('m')]
+        public void NotAllowPlayerToPlaceOtherTokenThanXOrO(char token)
         {
             Board board = new Board();
 
-            Action action = () => board.PlaceToken('d', 0, 0);
+            Action action = () => board.PlaceToken(token, 0, 0);
 
             Assert.Throws<ArgumentException>(action);
         }
-        
-        [Fact]
-        public void NotAllowPlayerToPlaceTokenM()
-        {
-            Board board = new Board();
-
-            Action action = () => board.PlaceToken('m', 0, 0);
-
-            Assert.Throws<ArgumentException>(action);
-        }
-
     }
 }
