7b5ec8429d4b52a5c90fe68255efc20f2c6e83e5
vkovaleva <vkovaleva@voxelgroup.net>
2021-12-13 15:14:52 +0100

    feat: create _playingToken field and add conditions for different tokens

--- TicTacToeKata/Board.cs
+++ TicTacToeKata/Board.cs
@@ -2,13 +2,20 @@
 {
     public class Board
     {
+        private char _playingToken;
+
         public object GetTokenAtPosition(int x, int y)
         {
-            return 'x';
+            return _playingToken;
         }
 
         public void PlaceToken(char token, int x, int y)
         {
+            if (token == 'o')
+                _playingToken = 'o';
+
+            if (token == 'x')
+                _playingToken = 'x';
         }
     }
 }
